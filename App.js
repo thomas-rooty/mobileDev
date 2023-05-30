8fdf22502311d0067219ea0867e1296a9caa5c13
tcaron <tc.caron91@gmail.com>
2023-05-30 09:38:48 +0200

    Removed default StyleSheet

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Armand s'optimise le megatron</Text>
+    <View>
+      <Text>Armand s'optimise le megatron !</Text>
       <StatusBar style="auto" />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
 
 export default App
